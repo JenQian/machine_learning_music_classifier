75b2d5377c1288cf7abe373c3ab8bed96d5649c2
Emma Pang <emma.kpang@gmail.com>
2020-04-15 21:03:22 -0400

    flask app edited

--- app.py
+++ app.py
@@ -1,6 +1,55 @@
 from flask import request, Flask, render_template
+import pickle
 
 app = Flask(__name__)
+
+# Load the model
+with open('pickle413.joblib', 'r') as pkl:    
+    model = pickle.load(pkl)
+
+genres={1: 'Movie',
+    2: 'R&B',
+    3: 'Dance',
+    4: 'Hip-Hop',
+    5: 'Pop',
+    6: 'Soul',
+    7: 'Indie',
+    8: 'Alternative',
+    9: 'Rap',
+    10: 'Folk',
+    11: 'Rock',
+    12: 'A Capella',
+    13: 'Country',
+    14: 'Blues',
+    15: 'Jazz',
+    16: 'Reggae',
+    17: 'World',
+    18: 'Electronic',
+    19: 'Reggaeton',
+    20: 'Ska',
+    21: 'Anime',
+    22: 'Soundtrack',
+    23: 'Classical',
+    24: 'Opera'}
+
+# helper function
+def use_model(user_input_song):
+    result_dictionary=get_requested_song_df(user_input_song)
+    prediction=model.predict(result_dictionary["model_df"])
+    # turn prediction into text
+    spotify_dict={
+        "user_song_genre":genres[prediction[0]],
+        "song_name":result_dictionary["song_name"],
+        "artist_name":result_dictionary["artist_name"],
+        "artwork":result_dictionary["artwork"],
+        "actual_genre":result_dictionary["actual_genre"],
+        "song_id":result_dictionary["song_id"]
+    }
+    return spotify_dict
+    
+    
+    
+
 
 @app.route("/")
 def index():
@@ -9,10 +58,16 @@ def index():
   
 
 @app.route("/handledata", methods=["POST"])
-def handledata():
-    print(request.form["input_song"])
 
-    return render_template("index.html", song_name=request.form["input_song"])
+def handledata():
+    
+    # use helper function to get result
+    song_name=use_model(request.form["input_song"])
+
+
+    return render_template("index.html", song_name=song_name)
+
+    
 
 
 
